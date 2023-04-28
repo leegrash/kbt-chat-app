5d6941ebb9ac6da6b752ac58c5908f04dd595dc4
Gustaf Svensson <gustaf.le.svensson@outlook.com>
2023-04-28 23:13:31 +0200

    some basic info updated

--- web-application/client/src/views/ShowTimeslots.vue
+++ web-application/client/src/views/ShowTimeslots.vue
@@ -10,22 +10,22 @@
                       <li class="clearfix">
                           <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar">
                           <div class="about">
-                              <div class="name">Vincent Porter</div>
-                              <div class="status"> <i class="fa fa-circle offline"></i> left 7 mins ago </div>                                            
+                              <div class="name">ChatBot#1</div>
+                              <div class="status">Closed Domain Chatbot</div>                                            
                           </div>
                       </li>
                       <li class="clearfix active">
                           <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="avatar">
                           <div class="about">
-                              <div class="name">Aiden Chavez</div>
-                              <div class="status"> <i class="fa fa-circle online"></i> online </div>
+                              <div class="name">ChatBot#2</div>
+                              <div class="status">Open Domain Chatbot</div>
                           </div>
                       </li>
                       <li class="clearfix">
                           <img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="avatar">
                           <div class="about">
-                              <div class="name">Mike Thomas</div>
-                              <div class="status"> <i class="fa fa-circle online"></i> online </div>
+                              <div class="name">ChatBot#3</div>
+                              <div class="status">Combined Chatbot</div>
                           </div>
                       </li>
                   </ul>
@@ -39,7 +39,6 @@
                               </a>
                               <div class="chat-about">
                                   <h6 class="m-b-0">Aiden Chavez</h6>
-                                  <small>Last seen: 2 hours ago</small>
                               </div>
                           </div>
                       </div>
@@ -58,135 +57,15 @@
                                   <span class="message-data-time">10:12 AM, Today</span>
                               </div>
                               <div class="message my-message">Are we meeting today?</div>                                    
-                          </li>                               
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:15 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data text-right">
-                                  <span class="message-data-time">10:10 AM, Today</span>
-                                  <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
-                              </div>
-                              <div class="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:12 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Are we meeting today?</div>                                    
-                          </li>                               
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:15 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data text-right">
-                                  <span class="message-data-time">10:10 AM, Today</span>
-                                  <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
-                              </div>
-                              <div class="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:12 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Are we meeting today?</div>                                    
-                          </li>                               
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:15 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data text-right">
-                                  <span class="message-data-time">10:10 AM, Today</span>
-                                  <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
-                              </div>
-                              <div class="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:12 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Are we meeting today?</div>                                    
-                          </li>                               
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:15 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data text-right">
-                                  <span class="message-data-time">10:10 AM, Today</span>
-                                  <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
-                              </div>
-                              <div class="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:12 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Are we meeting today?</div>                                    
-                          </li>                               
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:15 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data text-right">
-                                  <span class="message-data-time">10:10 AM, Today</span>
-                                  <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
-                              </div>
-                              <div class="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:12 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Are we meeting today?</div>                                    
-                          </li>                               
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:15 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Project has been already finished and I have results to show you.</div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data text-right">
-                                  <span class="message-data-time">10:10 AM, Today</span>
-                                  <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="avatar">
-                              </div>
-                              <div class="message other-message float-right"> Hi Aiden, how are you? How is the project coming along? </div>
-                          </li>
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:12 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Are we meeting today?</div>                                    
-                          </li>                               
-                          <li class="clearfix">
-                              <div class="message-data">
-                                  <span class="message-data-time">10:15 AM, Today</span>
-                              </div>
-                              <div class="message my-message">Project has been already finished and I have results to show you.</div>
                           </li>
                       </ul>
                   </div>
                   <div class="chat-message clearfix">
-                      <div class="input-group mb-0">
+                        <div class="input-group mb-0">
+                        <input type="text" class="form-control" placeholder="Enter text here...">
                           <div class="input-group-prepend">
                               <span class="input-group-text"><i class="fa fa-send"></i></span>
-                          </div>
-                          <input type="text" class="form-control" placeholder="Enter text here...">                                    
+                          </div>                                    
                       </div>
                   </div>
               </div>

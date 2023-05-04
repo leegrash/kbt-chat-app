5218e9dbc2d039b7d681230ff6bd43d4a4aa9ec8
Gustaf Svensson <gustaf.le.svensson@outlook.com>
2023-05-04 15:56:13 +0200

    added format of the req set

--- model-api/api-test.py
+++ model-api/api-test.py
@@ -8,8 +8,8 @@ def get_chatbot_response():
     data = request.get_json()
 
     # access the 'messages' and 'version' keys in the JSON object
-    messages = data['messages']
-    version = data['version']
+    messages = data['messages'] # format: [{'message': "Hi! I'm an AI Psychologist, how may I help you?", 'sender': 'bot'}, {'message': 'hi', 'sender': 'user'}]
+    version = data['version'] #string with text 'Closed' or 'Open' or 'Mixed'
 
     # define the 'response' variable
     response = "Hello, I am your chatbot."

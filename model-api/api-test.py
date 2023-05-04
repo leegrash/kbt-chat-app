cd8bb68201c14503a97f7a9fcade6e2cd60bffdf
Gustaf Svensson <gustaf.le.svensson@outlook.com>
2023-05-04 15:59:04 +0200

    clean code

--- model-api/api-test.py
+++ model-api/api-test.py
@@ -4,18 +4,16 @@ app = Flask(__name__)
 
 @app.route('/chatbot', methods=['POST'])
 def get_chatbot_response():
-    # get the JSON object from the request
     data = request.get_json()
 
-    # access the 'messages' and 'version' keys in the JSON object
     messages = data['messages'] # format: [{'message': "Hi! I'm an AI Psychologist, how may I help you?", 'sender': 'bot'}, {'message': 'hi', 'sender': 'user'}]
     version = data['version'] #string with text 'Closed' or 'Open' or 'Mixed'
 
-    # define the 'response' variable
+
     response = "Hello, I am your chatbot."
     print(response)
 
-    return jsonify({'response': response})
+    return jsonify({'response': response, 'title': "Test chat"})
 
 if __name__ == '__main__':
     app.run()

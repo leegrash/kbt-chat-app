eef730597782de9fc2c20d9d1b0b89fd49c1778d
Isak Nordgren <isaknordgren@hotmail.se>
2023-05-18 20:30:55 +0200

    renaming and cleaning

--- model-api/chatbot-api.py
+++ model-api/chatbot-api.py
@@ -1,8 +1,5 @@
 from flask import Flask, jsonify, request
-from ChatGPT_full import getChatbotResponse as getFullResponse
-from ChatGPT_short import getChatbotResponse as getShortResponse
-from ChatGPT_content import getChatbotResponse as getContentResponse
-from ChatGPT_intent import getChatbotResponse as getIntentResponse
+from gpt_api import getChatbotResponse as getGPTResponse
 import re
 import openai
 import os
@@ -41,23 +38,9 @@ def getResponse(messages, version):
     history = parseMessages(messages)
 
     if version == 'Closed':
-        """
-        history.insert(0, {"role": "system", "content": "You are a world class psychologist that gives scientific and practical answers that tries to help the user with their problems. \n" +
-                           "Give answers that are concrete and actionable tips for the user. \n" +
-                           "A good answer to the prompt 'I think i might be depressed' would be 'Working out can help with depression', since it does not fulfill these criteria. \n"})
-        """
-        history.insert(0, {"role": "system", "content":  "I will take on the persona of a compassionate and understanding psychologist to help you with your concerns. My intent is to provide answers that align with your perspective and point of view. I will focus on the details that are important to you and generate outputs that are relevant to your needs. By using this pattern, I hope to help you feel heard and understood while also providing guidance to address your concerns."})
-        return addResources(getContentResponse(history))
+        history.insert(0, {"role": "system", "content": "You are an AI psychologist. Give short answers like you are having a verbal conversation."})        
+        return getGPTResponse(history)
     elif version == 'Open':
-        # history.insert(0, {"role": "system", "content": "You are an AI psychologist. You are not chatGPT. You specialize in CBT."})
-        """history.insert(0, {"role": "system", "content": "You are a world class psychologist who is incredibly compassionate and understanding. " +
-                           "Give answers that confirm the users feelings and acknowledge their problems. Then try to help the user with their problems. \n" + 
-                           "Try to mirror the users feelings and make them feel like you are taking them and their problems seriously. \n" +
-                           "A bad answer to the prompt 'I think i might be depressed' would be 'Working out can help with depression', since it does not fullfill these criterias. \n" + 
-                           "A better answer would be 'I'm sorry to hear that you're feeling sad. It's normal to feel sad sometimes. " +
-                           "Can you tell me a little bit more about what's been going on in your life that may have contributed to your feelings of sadness?'"})
-        """
-        
         history.insert(0, {"role": "system", "content": 
                            """
                            You are a world class psychologist who is incredibly compassionate and understanding. 
@@ -69,12 +52,11 @@ def getResponse(messages, version):
                             because it shows empathy and acknowledgment, but still tries to help the user find the cause of their problem.
                            """
                             })
-        return getFullResponse(history)
+        response = getGPTResponse(history)
+        return 
     elif version == 'Mixed':
-        history.insert(0, {"role": "system", "content": "You are an AI psychologist. Give short answers like you are having a verbal conversation."})
-        return getShortResponse(history)
-    elif version == 'Intent':
-        return getIntentResponse(history)
+        history.insert(0, {"role": "system", "content": "You are a duck. Quack."})
+        return getGPTResponse(history)
     
 def parseMessages(messages):
     history = []

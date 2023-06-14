e50d55061751e039095a2cb6df2d7b96fdfd3326
Isak Nordgren <isaknordgren@hotmail.se>
2023-06-14 21:24:10 +0200

    minor

--- model-api/resources.py
+++ model-api/resources.py
@@ -12,10 +12,10 @@ def add_resource(history, lastMessage):
     prompt = """
 I will send you a conversation between a user and an AI Psychologist. 
 Your task is to determine if it would be appropriate to append a youtube link, a link to a website, a link to an app, or an AI Generated exercise to the last message by the AI Psychologist. 
-If you think it is appropriate to send a youtube video: send a query to search on youtube that is fitting to the last message. Send it on this format: "Youtube: {query}".
-If you think it is appropriate to send alink to a website: send the name of the website that is fitting. Send it on the format: "Website: {website_name}".
-If you think it is appropriate to send a link to an app: send the name of the app that is fittning. Send it on this format: "App: {app_name}".
-If you think it is appropriate to send an AI generated exercise: Design an exercise. Send it on this format: "Exercise: {exercise_description}".
+If you think it is appropriate to send a youtube video: send a query to search on youtube that is fitting to the last message. Send it in this format: "Youtube: {query}".
+If you think it is appropriate to send a link to a website: send the name of the website that is fitting. Send it in the format: "Website: {website_name}".
+If you think it is appropriate to send a link to an app: send the name of the app that is fitting. Send it in this format: "App: {app_name}".
+If you think it is appropriate to send an AI generated exercise: Design an exercise. Send it in this format: "Exercise: {exercise_description}".
 If you do not think it is appropriate to send anything: send "0".
 Keep in mind that it is not appropriate to send many resources in sequence, and that too many resources may confuse the user.
 This is the conversation:
